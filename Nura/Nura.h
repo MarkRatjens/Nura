cfec69ca3a365f736b99cd68df3ee7f5134d1783
Mark Ratjens <mark@ratjens.com>
2019-03-18 15:29:07 +1100

    Initial Commit

---
+++ Nura/Nura.h
@@ -0,0 +1,19 @@
+//
+//  Nura.h
+//  Nura
+//
+//  Created by Mark Ratjens on 18/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nura.
+FOUNDATION_EXPORT double NuraVersionNumber;
+
+//! Project version string for Nura.
+FOUNDATION_EXPORT const unsigned char NuraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nura/PublicHeader.h>
+
+
